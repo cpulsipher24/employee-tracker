22553ce476e3da4f51db7a39f3312abd6045a819
Cpulsipher24 <collintpulsipher@gmail.com>
2024-01-30 19:57:18 -0700

    Added functions for getting roles and employees

--- queries.js
+++ queries.js
@@ -10,3 +10,25 @@ async function getAllDepartments() {
     throw error;
   }
 }
+
+// Function to get all roles
+async function getAllRoles() {
+    try {
+      const [rows, fields] = await db.query('SELECT * FROM role');
+      return rows;
+    } catch (error) {
+      console.error('Error in getAllRoles:', error);
+      throw error;
+    }
+  }
+  
+  // Function to get all employees
+  async function getAllEmployees() {
+    try {
+      const [rows, fields] = await db.query('SELECT * FROM employee');
+      return rows;
+    } catch (error) {
+      console.error('Error in getAllEmployees:', error);
+      throw error;
+    }
+  }
