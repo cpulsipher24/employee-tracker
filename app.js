40b2a4a2a9b2f1a085bac8891c83c496dda8e3de
Cpulsipher24 <collintpulsipher@gmail.com>
2024-01-30 21:51:00 -0700

    Added functions for viewing departments, roles, and employees

--- app.js
+++ app.js
@@ -3,52 +3,78 @@ const queries = require('./queries'); // Adjust the path if needed
 
 // Main function to start the application
 async function startApp() {
-    console.log('Welcome to the Employee Tracker!');
-  
-    while (true) {
-        // Prompt user for action
-        const { action } = await inquirer.prompt({
-          name: 'action',
-          type: 'list',
-          message: 'What would you like to do?',
-          choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit'
-          ],
-        });
-    
-// Perform the selected action
-switch (action) {
-    case 'View all departments':
-      viewAllDepartments();
-      break;
-    case 'View all roles':
-      viewAllRoles();
-      break;
-    case 'View all employees':
-      viewAllEmployees();
-      break;
-    case 'Add a department':
-      await addDepartment();
-      break;
-    case 'Add a role':
-      await addRole();
-      break;
-    case 'Add an employee':
-      await addEmployee();
-      break;
-    case 'Update an employee role':
-      await updateEmployeeRole();
-      break;
-    case 'Exit':
-      console.log('Exiting the Employee Tracker. Goodbye!');
-      process.exit();
+  console.log('Welcome to the Employee Tracker!');
+
+  while (true) {
+    // Prompt user for action
+    const { action } = await inquirer.prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all departments',
+        'View all roles',
+        'View all employees',
+        'Add a department',
+        'Add a role',
+        'Add an employee',
+        'Update an employee role',
+        'Exit'
+      ],
+    });
+
+    // Perform the selected action
+    switch (action) {
+      case 'View all departments':
+        viewAllDepartments();
+        break;
+      case 'View all roles':
+        viewAllRoles();
+        break;
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'Add a department':
+        await addDepartment();
+        break;
+      case 'Add a role':
+        await addRole();
+        break;
+      case 'Add an employee':
+        await addEmployee();
+        break;
+      case 'Update an employee role':
+        await updateEmployeeRole();
+        break;
+      case 'Exit':
+        console.log('Exiting the Employee Tracker. Goodbye!');
+        process.exit();
+    }
   }
 }
+
+// Function to view all departments
+async function viewAllDepartments() {
+  const departments = await queries.getAllDepartments();
+  console.table(departments);
 }
+
+// Function to view all roles
+async function viewAllRoles() {
+  const roles = await queries.getAllRoles();
+  console.table(roles);
+}
+
+// Function to view all employees
+async function viewAllEmployees() {
+  const employees = await queries.getAllEmployees();
+  console.table(employees);
+}
+
+// Function to add a department
+async function addDepartment() {
+  const { departmentName } = await inquirer.prompt({
+    name: 'departmentName',
+    type: 'input',
+    message: 'Enter the name of the department:',
+  });
